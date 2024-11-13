ed370594f84c95dad51251055ef26724cef12c76
Ben Gurovich <benjaguro@gmail.com>
2024-11-12 17:03:49 -0700

    Initial commit

---
+++ test.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+    int age;
+    printf("How old are you: ");
+    scanf("%d", &age);
+    printf("You are %d years old!!", age);
+    return 0;
+}
+
+//TEST TEST
+
+
+// BRYAN CODE
+//FUCNTION 1 
+//FUNCION XX
+
+
+
+
+
+
+
+
+
+
+
+
+
+// BENS CODE
+
+
+
+
+
+
+
+// XX CODE
