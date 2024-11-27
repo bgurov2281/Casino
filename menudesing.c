b3f96350cd3f017d99721f28c447cefd32135eb3
Ben Gurovich <benjaguro@gmail.com>
2024-11-26 23:51:51 -0700

    aethetics

---
+++ menudesing.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main(){
+    printf("\n");
+    printf("******************************************\n");
+    printf("*                                        *\n");
+    printf("*           $$ OHM'S CASINO $$           *\n");
+    printf("*                                        *\n");
+    printf("******************************************\n");
+    printf("*                                        *\n");
+    printf("*           1. Play Slots                *\n");
+    printf("*           2. Play Blackjack            *\n");
+    printf("*           3. Play Roulette             *\n");
+    printf("*           4. Play Craps                *\n");
+    printf("*           4. View Tokens               *\n");
+    printf("*           5. Exit                      *\n");
+    printf("*                                        *\n");
+    printf("******************************************\n");
+
+}
